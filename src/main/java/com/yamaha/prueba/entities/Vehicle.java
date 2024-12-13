27d5a7e752c14368372956a66c86d8547d24aa5b
JuanPabaz <jpgthf@gmail.com>
2024-12-13 09:03:38 -0500

    feat: add Vehicle entity

---
+++ src/main/java/com/yamaha/prueba/entities/Vehicle.java
@@ -0,0 +1,44 @@
+package com.yamaha.prueba.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "vehicles")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long vehicleId;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String engineNumber;
+
+    @Column(nullable = false, length = 50)
+    private String model;
+
+    @Column(nullable = false)
+    private Integer cylinderCapacity;
+
+    @Column(nullable = false, length = 30)
+    private String color;
+
+    @Column(nullable = false)
+    private LocalDate dateOfAssembly;
+
+    @Column(nullable = false)
+    private Integer yearModel;
+
+    @OneToMany(mappedBy = "vehiculo", cascade = CascadeType.ALL)
+    private List<Sales> ventas;
+
+    // Getters and Setters
+}
