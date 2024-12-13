823f1c6fbd15677eea5ca3aec1de9b97bc8c17ff
JuanPabaz <jpgthf@gmail.com>
2024-12-13 09:02:28 -0500

    feat: add Client entity

---
+++ src/main/java/com/yamaha/prueba/entities/Client.java
@@ -0,0 +1,50 @@
+package com.yamaha.prueba.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "clients")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long clientId;
+
+    @Column(nullable = false, unique = true, length = 20)
+    private String identificationNumber;
+
+    @Column(nullable = false, length = 100)
+    private String names;
+
+    @Column(nullable = false, length = 100)
+    private String lastName;
+
+    @Column(length = 255)
+    private String address;
+
+    @Column(nullable = false)
+    private LocalDate dateOfBirth;
+
+    @Column(nullable = false, length = 1)
+    private String gender;
+
+    @Column(nullable = false, length = 15)
+    private String cellPhone;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
+    private List<Sales> sales;
+
+    // Getters and Setters
+}
