82451138cf2abb1729f06b25e475292eeccb68bd
JuanPabaz <jpgthf@gmail.com>
2024-12-13 09:25:15 -0500

    feat: create AuthenticationService

---
+++ src/main/java/com/yamaha/prueba/service/AuthenticationService.java
@@ -0,0 +1,69 @@
+package com.yamaha.prueba.service;
+
+import com.yamaha.prueba.dto.RegisterRequestDTO;
+import com.yamaha.prueba.dto.UserResponseDTO;
+import com.yamaha.prueba.entities.User;
+import com.yamaha.prueba.enums.Role;
+import com.yamaha.prueba.exceptions.BadUserCredentialsException;
+import com.yamaha.prueba.exceptions.ObjectNotFoundException;
+import com.yamaha.prueba.maps.IMapUser;
+import com.yamaha.prueba.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final JwtServiceImpl jwtService;
+
+    private final IMapUser mapUser;
+
+    public AuthenticationService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtServiceImpl jwtService, IMapUser mapUser) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
+        this.mapUser = mapUser;
+    }
+
+
+    public UserResponseDTO saveUser(RegisterRequestDTO registerRequestDTO) throws BadUserCredentialsException {
+        if (userRepository.findByUsername(registerRequestDTO.getUsername()).isPresent()){
+            throw new BadUserCredentialsException("Ya existe un usuario con este correo: "+ registerRequestDTO.getUsername() + ".");
+        }
+
+        String passwordRegex = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d/*\\-_.º!?¿'¡#!$%&]{6,}$";
+        if (!registerRequestDTO.getPassword().matches(passwordRegex)){
+            throw new BadUserCredentialsException("La contraseña debe tener al menos 6 caracteres y contener al menos una letra y un número.");
+        }
+        String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+        if (!registerRequestDTO.getUsername().matches(emailRegex)){
+            throw new BadUserCredentialsException("El correo no es valido.");
+        }
+
+        registerRequestDTO.setRole(Role.ADMIN);
+        registerRequestDTO.setPassword(passwordEncoder.encode(registerRequestDTO.getPassword()));
+
+        User user = User.builder()
+                .username(registerRequestDTO.getUsername())
+                .password(passwordEncoder.encode(registerRequestDTO.getPassword()))
+                .role(registerRequestDTO.getRole())
+                .fullName(registerRequestDTO.getFullName())
+                .build();
+
+        return mapUser.mapUser(user);
+    }
+
+    public String generateToken(String username) throws ObjectNotFoundException {
+        return jwtService.generateToken(username);
+    }
+
+    public Map<String, Object> validateToken(String token){
+        return jwtService.validateToken(token);
+    }
+}
