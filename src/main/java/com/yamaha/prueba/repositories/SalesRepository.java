d1cc8a4e81d24f37c978359c616e1e638bef8767
JuanPabaz <jpgthf@gmail.com>
2024-12-13 13:02:02 -0500

    feat: add findClientsWithMoreThanTwoSales

--- src/main/java/com/yamaha/prueba/repositories/SalesRepository.java
+++ src/main/java/com/yamaha/prueba/repositories/SalesRepository.java
@@ -1,7 +1,14 @@
 package com.yamaha.prueba.repositories;
 
+import com.yamaha.prueba.entities.Client;
 import com.yamaha.prueba.entities.Sales;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SalesRepository extends JpaRepository<Sales, Integer> {
+
+    @Query("SELECT c FROM Client c JOIN c.sales s GROUP BY c.clientId HAVING COUNT(s.saleId) > 2")
+    List<Client> findClientsWithMoreThanTwoSales();
 }
