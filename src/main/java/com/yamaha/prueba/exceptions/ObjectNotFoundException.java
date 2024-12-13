531b99c289f0f50e3b1cfaf555b143b2305ff55e
JuanPabaz <jpgthf@gmail.com>
2024-12-13 09:22:56 -0500

    feat: add ObjectNotFoundException

---
+++ src/main/java/com/yamaha/prueba/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.yamaha.prueba.exceptions;
+
+public class ObjectNotFoundException extends Exception{
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+}
